eba5740734c74091e89e5fd0e98152973746b75f
Brazilyy <mohammedamr200497@gmail.com>
2025-12-09 03:03:40 +0200

    Time: 535 ms (74.08%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,9 +1,3 @@
-SELECT name, population, area 
-FROM World
-WHERE area >= 3000000
-
-UNION
-
-SELECT name, population, area 
-FROM World
-WHERE population >= 25000000;
+/* Write your T-SQL query statement below */
+SELECT name,population ,area FROM World 
+Where area >=3000000 or population >=25000000  
